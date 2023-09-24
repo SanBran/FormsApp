4a1a10c46d07520888bbcbb41794b4e6c1620bbe
SanBran <luminoforos@gmail.com>
2023-09-24 09:21:30 -0600

    Deploy

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000";
-// axios.defaults.baseURL = "";
+// axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://forms-app-chi.vercel.app/";
 
 export default axios;
