9999dc86256272536f3235a86f6602714bec1817
SanBran <luminoforos@gmail.com>
2023-09-24 10:20:03 -0600

    Correción cors

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:8000";
-axios.defaults.baseURL = "forms-g2h8w88x0-sanbran.vercel.app";
+axios.defaults.baseURL = process.env.URL_BASE;
 
 export default axios;
