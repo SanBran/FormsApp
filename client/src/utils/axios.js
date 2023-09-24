8983af0478e7ebe01a96f81c0a04688c084bbfd8
SanBran <luminoforos@gmail.com>
2023-09-24 11:00:59 -0600

    Correción2

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:8000";
-axios.defaults.baseURL = process.env.URL_BASE;
+axios.defaults.baseURL = "https://forms-app-chi.vercel.app/";
 
 export default axios;
