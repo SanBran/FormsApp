24d5e6fa8b9b84ca767b43726e928d63e622e8fe
SanBran <luminoforos@gmail.com>
2023-09-25 20:25:19 -0600

    Correción api

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:8000";
-axios.defaults.baseURL = "https://form-api-iota.vercel.app/";
+axios.defaults.baseURL = "https://forms-app-pied.vercel.app/";
 
 export default axios;
